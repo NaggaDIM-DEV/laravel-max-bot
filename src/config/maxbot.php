9287ffbd1e61dce0fc705560d70ed50a837b68b5
NaggaDIM <naggadim@vk.com>
2026-02-12 11:34:38 +0300

    v0.1.0 - readme

--- src/config/maxbot.php
+++ src/config/maxbot.php
@@ -4,4 +4,8 @@ return [
     'debug' => false,
     'api_url' => 'https://platform-api.max.ru',
     'token' => env('MAX_MESSENGER_BOT_TOKEN'),
+    'long-polling'  => [
+        'limit' => 100,
+        'timeout' => 30,
+    ]
 ];
